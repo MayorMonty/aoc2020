78aa0dc8e4053ebd663a320737c2c17149cf27a3
Brendan McGuire <brendan@bren.app>
2020-12-09 18:36:56 -0500

    Custom delimiter

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -2,7 +2,7 @@ use crate::input;
 use std::io;
 
 pub fn run() -> io::Result<usize> {   
-    let items = input::from_file::<usize>(&"src/day1/input.txt")?;
+    let items = input::from_file::<usize>(&"src/day1/input.txt", b'\n')?;
 
     for a in &items {
         for b in &items {
--- src/input.rs
+++ src/input.rs
@@ -6,7 +6,7 @@ use std::{
     str::FromStr,
 };
 
-pub fn from_file<T: FromStr>(filename: &'static str) -> io::Result<Vec<T>>
+pub fn from_file<T: FromStr>(filename: &'static str, delimiter: u8) -> io::Result<Vec<T>>
 where
     <T as std::str::FromStr>::Err: std::fmt::Debug,
 {
@@ -14,7 +14,7 @@ where
     let file = File::open(&path)?;
 
     // Read the file line by line
-    let lines = BufReader::new(file).lines();
+    let lines = BufReader::new(file).split(delimiter).map(|l| String::from_utf8(l.unwrap()));
 
     // Create a vector to hold the numbers, with max capacity of the lower bound
     let mut items: Vec<T> = Vec::with_capacity(lines.size_hint().0);
